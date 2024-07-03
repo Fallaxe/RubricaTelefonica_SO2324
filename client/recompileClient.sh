91630765eee9af504b4b35e541bc262960b8dc91
0xDani <daniele.fallaci@gmail.com>
2024-07-03 12:44:58 +0200

    bye bye recompile

--- client/recompileClient.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# -*- coding: utf-8 -*-
-# compile client more quickly
-mkdir -p ../build;
-gcc -c ./client.c -o ../build/client.o;
-mkdir -p ../bin;
-gcc ../build/client.o -o ../bin/client;
-../bin/client;
